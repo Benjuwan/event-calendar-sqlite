d41fcdaf936899525387f126ee364b928255d9e5
benjuwan <benjuwan.banquet12@gmail.com>
2025-11-30 07:10:45 +0900

    fix: 各種ライブラリのアップデート・アップグレード及びREADMEの更新、一部リファクタリング.

--- src/components/rooms/components/ViewCurrentTimeTableDay.tsx
+++ src/components/rooms/components/ViewCurrentTimeTableDay.tsx
@@ -10,7 +10,7 @@ function ViewCurrentTimeTableDay({ ctrlMultiTimeTable, isLastWeek }: { ctrlMulti
     return (
         <>
             {(pathName.length === 1 || pathName === `/${ADMIN_PAGE_PATH_NAME}`) &&
-                <p suppressHydrationWarning={true}>- <b>{isLastWeek && ctrlMultiTimeTable <= 7 ? thisMonth + 1 : thisMonth}/{ctrlMultiTimeTable}</b> の予約内容（※7日後まで確認可能）</p>
+                <p>- <b>{isLastWeek && ctrlMultiTimeTable <= 7 ? thisMonth + 1 : thisMonth}/{ctrlMultiTimeTable}</b> の予約内容（※7日後まで確認可能）</p>
             }
         </>
     );
